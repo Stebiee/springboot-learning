d610cf808db6b2a52b0ed313edb37703faae9ca4
Stebiee <stebie11mad@gmail.com>
2024-03-28 17:31:22 -0700

    .

---
+++ spring/spring-jdbc-jpa/tennis-player2/src/main/java/stebie/tennisplayer/TennisPlayerApplication.java
@@ -0,0 +1,42 @@
+package stebie.tennisplayer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TennisPlayerApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PlayerDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisPlayerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+			testPlayerByNationality();
+	}
+
+	private void testDelete() {
+		// deleting a player
+		logger.info("\nBefore delete: {}", dao.getAllPlayers());
+		logger.info("Deleting Player with ID 2: {}", dao.deletePlayerById(2));
+		logger.info("\nAfter delete: {}", dao.getAllPlayers());
+	}
+
+	private void testTableCreation() {
+		dao.createTournamentTable();
+	}
+
+	private void testPlayerByNationality() {
+		logger.info("\nFrench Players2: {}", dao.getPlayerByNationality("France"));
+	}
+}
---
+++ spring/spring-jdbc-jpa/tennis-player2/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Player (
+    ID INTEGER NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Nationality VARCHAR(255) NOT NULL,
+    Birth_Date TIMESTAMP,
+    Titles INTEGER,
+    PRIMARY KEY (ID)
+);
