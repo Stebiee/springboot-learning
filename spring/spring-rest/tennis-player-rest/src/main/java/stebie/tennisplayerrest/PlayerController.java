6d816bda7ae78c81fac76cf83822eda2c363acd7
Stebiee <stebie11mad@gmail.com>
2024-04-13 10:30:37 -0700

    .

--- spring/spring-rest/tennis-player-rest/src/main/java/stebie/tennisplayerrest/PlayerController.java
+++ spring/spring-rest/tennis-player-rest/src/main/java/stebie/tennisplayerrest/PlayerController.java
@@ -1,13 +1,34 @@
 package stebie.tennisplayerrest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PlayerController {
+    @Autowired
+    PlayerService service;
 
     @GetMapping("/test-api")
     public String welcome() {
         return "REST API is working";
+    }
+
+    @GetMapping("/players")
+    public List<Player> getAllPlayers() {
+        return service.getAllPlayers();
+    }
+
+    @PostMapping("/players")
+    public Player addPlayer(@RequestBody Player p) {
+        p.setId(0);
+
+        return service.addPlayer(p);
+    }
+
+    @GetMapping("/players/{id}")
+    public Player getPlayer(@PathVariable("id") int playerId) {
+        return service.getPlayerById(playerId);
     }
 }
--- spring/spring-rest/tennis-player-rest/src/main/java/stebie/tennisplayerrest/PlayerService.java
+++ spring/spring-rest/tennis-player-rest/src/main/java/stebie/tennisplayerrest/PlayerService.java
@@ -2,8 +2,10 @@ package stebie.tennisplayerrest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
@@ -16,8 +18,23 @@ public class PlayerService {
     }
 
     //Get player by ID
+    public Player getPlayerById(int id) {
+        Optional<Player> tempPlayer = repo.findById(id);
+        Player p = null;
+
+        if (tempPlayer.isPresent()) {
+            p = tempPlayer.get();
+        } else {
+            throw new RuntimeException("Player id " + id + " not found");
+        }
+
+        return p;
+    }
 
     //Add a player
+    public Player addPlayer(Player p) {
+        return repo.save(p);
+    }
 
     //Update a player
 
